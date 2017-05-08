67a2d6f53db6deba6d3b219a23ce6eef9cb26aeb
epth <ckleoric@gmail.com>
2017-05-08 08:48:36 +0800

    channels

--- apps/views.py
+++ apps/views.py
@@ -3,7 +3,9 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from download.models import DOWNLOAD
 
 # Create your views here.
 def index(request):
-    return render(request,'apps/index.html')
+    all_sugars=DOWNLOAD.objects.all()
+    return render(request,'apps/index.html',locals())
